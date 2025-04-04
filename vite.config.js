00426fb422bc1fd9520aa79499bb01cebe65949d
Yarov Rivera <yarovrr@gmail.com>
2025-04-03 23:57:03 -0600

    init app

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/infraccion-cdmx/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    cssMinify: false
   },
   plugins: [react()]
 })
