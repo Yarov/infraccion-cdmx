4045e13c9155f81ab99e56a14dc59f8ccb5621b6
Yarov Rivera <yarovrr@gmail.com>
2025-04-04 00:31:04 -0600

    update css

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/infraccion-cdmx/',
   build: {
-    outDir: 'dist',
-    cssMinify: false
+    outDir: 'dist'
   },
   plugins: [react()]
 })
